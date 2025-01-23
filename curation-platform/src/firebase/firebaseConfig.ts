aee6cf76c9ed12a05082534b376daa0d3963ed27
WinnieLam <winnie.lin.1225@gmail.com>
2025-01-23 10:53:12 +0000

    move sensitive data to .gitignore

--- curation-platform/src/firebase/firebaseConfig.ts
+++ curation-platform/src/firebase/firebaseConfig.ts
@@ -4,13 +4,13 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCZBAemiYHn-l11Wm5-C7yj1GRZHIQVJRo",
-  authDomain: "exhibition-curation-platform.firebaseapp.com",
-  projectId: "exhibition-curation-platform",
-  storageBucket: "exhibition-curation-platform.firebasestorage.app",
-  messagingSenderId: "276870702725",
-  appId: "1:276870702725:web:24ee6b0a71547a8e9eabec",
-  measurementId: "G-1MB8LW0EFP"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
